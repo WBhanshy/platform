aa3715b06b10c43324ecb1ec80c2bbf843f4812c
Brandon Farmer <bthesorceror@gmail.com>
2018-12-12 14:01:43 -0800

    Switch buckets ui to use generated client

--- ui/src/shared/apis/v2/buckets.ts
+++ ui/src/shared/apis/v2/buckets.ts
@@ -1,10 +1,11 @@
-import AJAX from 'src/utils/ajax'
-import {Bucket} from 'src/types/v2/buckets'
+import {Bucket} from 'src/api'
+import {bucketsAPI} from 'src/utils/api'
 
-export const getBuckets = async (url: string): Promise<Bucket[]> => {
+export const getBuckets = async (): Promise<Bucket[]> => {
   try {
-    const {data} = await AJAX({url})
-    return data
+    const {data} = await bucketsAPI.bucketsGet('')
+
+    return data.buckets
   } catch (error) {
     console.error(error)
     throw error
